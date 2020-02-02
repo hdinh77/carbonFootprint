22ef752026b0f8add49238c2933005865e30f0b8
hdinh77 <heatherdinh11@gmail.com>
2020-02-01 23:56:10 -0800

    finishing the resources

--- heather/app/src/main/java/com/example/heat/MainActivity.java
+++ heather/app/src/main/java/com/example/heat/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.heat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
